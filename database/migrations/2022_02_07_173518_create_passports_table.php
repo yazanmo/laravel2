886475536b18ece823ce25e3c182001c184a311b
yazanmo <yazanmostafa96@gmail.com>
2022-02-07 21:18:01 +0200

    done

---
+++ database/migrations/2022_02_07_173518_create_passports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePassportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passports', function (Blueprint $table) {
+            $table->id();
+            $table->string('passport_no');
+            $table->string('country');
+            $table->string('gender');
+            $table->date('expiry_date');
+ 
+            // set up relations
+            $table->unsignedBigInteger('passenger_id');
+            $table->foreign('passenger_id')->references('id')->on('passengers')->onDelete('CASCADE')->onUpdate('CASCADE');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passports');
+    }
+}
